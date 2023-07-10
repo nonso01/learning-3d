e4672de9ea413f82d0729d11dfdc3657e3a16292
nonso01 <chukwunonsomartin01@gmail.com>
2023-07-10 15:47:09 +0100

    (#000)

--- src/main.js
+++ src/main.js
@@ -1,38 +1,43 @@
-import * as T from "three"
-import eruda from "eruda"
-import { log, w, dq } from "./util"
+import * as T from "three";
+import eruda from "eruda";
+import { log, w, dq } from "./util";
 
-//eruda.init()
+eruda.init();
 
-const app = dq("#app")
+const app = dq("#app");
 
+const Scene = new T.Scene();
+const Camera = new T.PerspectiveCamera(
+  75,
+  w.innerWidth / w.innerHeight,
+  0.1,
+  1000,
+);
+const Renderer = new T.WebGLRenderer();
 
-const Scene = new T.Scene()
-const Camera = new T.PerspectiveCamera(75, w.innerWidth / w.innerHeight, .1, 1000)
-const Renderer = new T.WebGLRenderer()
+Renderer.setSize(w.innerWidth, w.innerHeight);
 
-Renderer.setSize(w.innerWidth, w.innerHeight)
+app.append(Renderer.domElement);
 
-app.append(Renderer.domElement)
+const light = new T.DirectionalLight(0xffffff, 2);
 
-const light = new T.DirectionalLight(0xffffff, 2)
-
-const boxGeo = new T.BoxGeometry(1,1,1);
-const boxMesh = new T.MeshStandardMaterial({
+const boxGeo = new T.BoxGeometry(1, 2, 1);
+/*const boxMesh = new T.MeshStandardMaterial({
 	color: 0x424242
-})
-const box = new T.Mesh(boxGeo, boxMesh)
+})*/
+const boxMesh = new T.MeshNormalMaterial();
 
+const box = new T.Mesh(boxGeo, boxMesh);
 
-for( const s of [box, light]) Scene.add(s)
+for (const s of [box, light]) Scene.add(s);
 
-Camera.position.z = 5
+Camera.position.z = 5;
 
 function animate() {
-	requestAnimationFrame(animate)
-	Renderer.render(Scene, Camera)
+  requestAnimationFrame(animate);
+  Renderer.render(Scene, Camera);
 
-	box.rotation.x += 0.01
-	box.rotation.y += .01
+  box.rotation.x += 0.01;
+  box.rotation.y += 0.01;
 }
-animate()
+animate();
--- src/util.js
+++ src/util.js
@@ -1,5 +1,5 @@
-export const [w, d, log] = [window, document, console.log]
+export const [w, d, log] = [window, document, console.log];
 
 export function dq(x = "") {
-  return d.querySelector(x)
+  return d.querySelector(x);
 }
